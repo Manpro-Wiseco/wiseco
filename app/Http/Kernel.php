86d094c3d494922ec036224649feac9831b54826
Muqiit <mmuqiit.f14@gmail.com>
2022-03-09 22:12:47 +0700

    check-company-session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CheckCompanySession;
 use App\Http\Middleware\CheckRole;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => CheckRole::class,
+        'company-session' => CheckCompanySession::class,
     ];
 }
