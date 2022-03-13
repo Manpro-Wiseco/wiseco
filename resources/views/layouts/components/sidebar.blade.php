74f29795dc29c7d40b2851b0818522b47949c9bf
Varez Yeyez <varez.yeyez@gmail.com>
2022-03-13 22:59:46 +0700

    minor update UI

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -12,7 +12,7 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link  active" href="{{ route('dashboard') }}">
+                <a @if(url()->current()==route('dashboard')) class="nav-link active" @else class="nav-link" @endif href="{{ route('dashboard') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1"
@@ -38,7 +38,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="{{ route('penjualan') }}">
+                <a @if(url()->current()==route('penjualan')) class="nav-link active" @else class="nav-link" @endif href="{{ route('penjualan') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
@@ -97,7 +97,7 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="#">
+                <a class="nav-link" href="#">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
@@ -123,7 +123,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="{{ route('inventory') }}">
+                <a @if(url()->current()==route('inventory')) class="nav-link active" @else class="nav-link" @endif href="{{ route('inventory') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
--- resources/views/user/inventory.blade.php
+++ resources/views/user/inventory.blade.php
@@ -2,136 +2,86 @@
     <section class="content">
         
         <div class="container">
-            {{-- <div class="row justify-content-md-center">
-                <div class="col">
-                    <div class="col-md-6">
-                        <div class="card bg-cover text-center" style="background-image: url('https://images.unsplash.com/photo-1541451378359-acdede43fdf4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80')">
-                        <div class="card-body z-index-2 py-7">
-                            <h3 class="text-white">Social Analytics</h3>
-                            <p class="text-white">
-                            Insight to help you create, connect.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="col-md-6">
-                        <div class="card bg-cover text-center" style="background-image: url('https://images.unsplash.com/photo-1541451378359-acdede43fdf4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80')">
-                        <div class="card-body z-index-2 py-7">
-                            <h3 class="text-white">Social Analytics</h3>
-                            <p class="text-white">
-                            Insight to help you create, connect.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="col-md-6">
-                        <div class="card bg-cover text-center" style="background-image: url('https://images.unsplash.com/photo-1541451378359-acdede43fdf4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80')">
-                        <div class="card-body z-index-2 py-7">
-                            <h3 class="text-white">Social Analytics</h3>
-                            <p class="text-white">
-                            Insight to help you create, connect.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="col-md-6">
-                        <div class="card bg-cover text-center" style="background-image: url('https://images.unsplash.com/photo-1541451378359-acdede43fdf4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80')">
-                        <div class="card-body z-index-2 py-7">
-                            <h3 class="text-white">Social Analytics</h3>
-                            <p class="text-white">
-                            Insight to help you create, connect.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-            
-{{-- --------------------------------------- --}}
             <div class="row">
                 <div class="col-md-4 mt-4">
-                    <div class="card text-center" style="background-color:rgb(121, 155, 219)">
-                        <div class="card-body py-7">
-                          <h3 class="text-Black">Data Produk</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                    <a class="link" href="#">
+                        <div class="card text-center" style="background-color:rgb(121, 155, 219)">
+                            <div class="card-body py-7">
+                              <h3 class="text-Black">Data Produk</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
                         </div>
-                      </div>
-                      
-                      
+                    </a>
                 </div>
             
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #E6BD9F">
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #E6BD9F">
                         <div class="card-body py-7">
                           <h3 class="text-black">Penyesuaian Barang</h3>
                           <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
                         </div>
                       </div>
-                      
-                      
+                    </a>
                 </div>
             
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #E69D93">
-                        <div class="card-body py-7">
-                          <h3 class="text-black">Stok Opname</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
-                        </div>
-                      </div>
-                      
-                      
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #E69D93">
+                            <div class="card-body py-7">
+                              <h3 class="text-black">Stok Opname</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
+                          </div>
+                    </a>
                 </div>
             
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #ADDBC6">
-                        <div class="card-body py-7">
-                          <h3 class="text-black">Pindah Gudang</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
-                        </div>
-                      </div>
-                      
-                      
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #ADDBC6">
+                            <div class="card-body py-7">
+                              <h3 class="text-black">Pindah Gudang</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
+                          </div>
+                    </a>
                 </div>
             
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #87ADE6">
-                        <div class="card-body py-7">
-                          <h3 class="text-black">Produksi</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
-                        </div>
-                      </div>
-                      
-                      
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #87ADE6">
+                            <div class="card-body py-7">
+                              <h3 class="text-black">Produksi</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
+                          </div>
+                    </a>
                 </div>
             
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #DEADE6">
-                        <div class="card-body py-7">
-                          <h3 class="text-black">Formula Produksi</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
-                        </div>
-                      </div>
-                      
-                      
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #DEADE6">
+                            <div class="card-body py-7">
+                              <h3 class="text-black">Formula Produksi</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
+                          </div>
+                    </a>
                 </div>
 
                 <div class="col-md-4 mt-4">
-                    <div class="card bg-cover text-center" style="background-color: #B7A1E6">
-                        <div class="card-body py-7">
-                          <h3 class="text-black">Konsinyasi Barang Keluar</h3>
-                          <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
-                        </div>
-                      </div>
-                      
-                      
+                    <a href="#" class="link">
+                        <div class="card bg-cover text-center" style="background-color: #B7A1E6">
+                            <div class="card-body py-7">
+                              <h3 class="text-black">Konsinyasi Barang Keluar</h3>
+                              <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut facilis mollitia, aperiam voluptate temporibus adipisci dolorem repellendus blanditiis voluptates assumenda rem</p>
+                            </div>
+                          </div>
+                    </a>
                 </div>
             
             </div>
   
-  
-
         </div>
     </section>
 </x-template-layout>
