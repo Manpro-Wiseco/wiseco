516f9d842157266ff85344063ee76cabea177bd0
Muqiit <mmuqiit.f14@gmail.com>
2022-04-04 16:30:04 +0700

    fund-transfer

---
+++ app/Http/Controllers/User/PengelolaanKas/FundTransferController.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Http\Controllers\User\PengelolaanKas;
+
+use App\Http\Controllers\Controller;
+use App\Models\BankAccount;
+use App\Models\FundTransfer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Yajra\DataTables\DataTables;
+
+class FundTransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('user.pengelolaan-kas.fund-transfer.index');
+    }
+
+    public function list(Request $request)
+    {
+        $data = FundTransfer::with(['fromBankAccounts', 'toBankAccounts'])->currentCompany()->latest()->get();
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->addColumn('from', function ($row) {
+                return $row->fromBankAccounts->name;
+            })
+            ->addColumn('to', function ($row) {
+                return $row->toBankAccounts->name;
+            })
+            ->addColumn('action', function ($row) {
+                $urlEdit = route('pengelolaan-kas.fund-transfer.edit', $row->id);
+                $urlShow = route('pengelolaan-kas.fund-transfer.show', $row->id);
+                $actionBtn = '
+                <a href="' . $urlShow . '" class="btn bg-gradient-success btn-small">
+                    <i class="fas fa-eye"></i>
+                </a>
+                <a href="' . $urlEdit . '" class="btn bg-gradient-info btn-small">
+                    <i class="fas fa-edit"></i>
+                </a>
+                
+    <button class="btn bg-gradient-danger btn-small btn-delete" data-id="' . $row->id . '" data-invoice="' . $row->invoice . '" type="button">
+        <i class="fas fa-trash"></i>
+    </button>';
+                return $actionBtn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    public function fromBank(Request $request)
+    {
+        $search = $request->search;
+        if ($search == '') {
+            $data = BankAccount::currentCompany()->get();
+        } else {
+            $data = BankAccount::currentCompany()->where('name', 'like', '%' . $search . '%')->get();
+        }
+        $response = array();
+        foreach ($data as $d) {
+            $response[] = array(
+                "id" => $d->id,
+                "text" => $d->name,
+            );
+        }
+        return response()->json($response);
+    }
+
+    public function toBank(Request $request)
+    {
+        $search = $request->search;
+        $fromBank = $request->fromBank;
+        if ($search == '') {
+            if ($fromBank) {
+                $data = BankAccount::currentCompany()->where('id', '!=', $fromBank)->get();
+            } else {
+                $data = BankAccount::currentCompany()->get();
+            }
+        } else {
+            if ($fromBank) {
+                $data = BankAccount::currentCompany()->where('name', 'like', '%' . $search . '%')->where('id', '!=', $fromBank)->get();
+            } else {
+                $data = BankAccount::currentCompany()->where('name', 'like', '%' . $search . '%')->get();
+            }
+        }
+        $response = array();
+        foreach ($data as $d) {
+            $response[] = array(
+                "id" => $d->id,
+                "text" => $d->name,
+            );
+        }
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.pengelolaan-kas.fund-transfer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'invoice' => 'required',
+            'description' => 'required',
+            'toBank' => 'required|numeric',
+            'fromBank' => 'required|numeric',
+            'total' => 'required|numeric',
+            'transaction_date' => 'required'
+        ]);
+
+        FundTransfer::create([
+            'invoice' => $request->invoice,
+            'description' => $request->description,
+            'from_bank_account' => $request->fromBank,
+            'to_bank_account' => $request->toBank,
+            'total' => $request->total,
+            'transaction_date' => $request->transaction_date,
+            'company_id' => session()->get('company')->id
+        ]);
+
+        return redirect()->route('pengelolaan-kas.fund-transfer.index')->with('success', 'Berhasil Menambahkan Data!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FundTransfer $fundTransfer)
+    {
+        return view('user.pengelolaan-kas.fund-transfer.edit', compact('fundTransfer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FundTransfer $fundTransfer)
+    {
+        $request->validate([
+            'invoice' => 'required',
+            'description' => 'required',
+            'toBank' => 'required|numeric',
+            'fromBank' => 'required|numeric',
+            'total' => 'required|numeric',
+            'transaction_date' => 'required'
+        ]);
+
+        $fundTransfer->update([
+            'invoice' => $request->invoice,
+            'description' => $request->description,
+            'from_bank_account' => $request->fromBank,
+            'to_bank_account' => $request->toBank,
+            'total' => $request->total,
+            'transaction_date' => $request->transaction_date
+        ]);
+
+        return redirect()->route('pengelolaan-kas.fund-transfer.index')->with('success', 'Berhasil Mengubah Data!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FundTransfer $fundTransfer)
+    {
+        $fundTransfer->delete();
+        return response()->json(['status' => TRUE]);
+    }
+}
---
+++ resources/views/user/pengelolaan-kas/fund-transfer/index.blade.php
@@ -0,0 +1,176 @@
+@push('styles')
+@endpush
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            let flashdatasukses = $('.success-session').data('flashdata');
+            if (flashdatasukses) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success!',
+                    text: flashdatasukses,
+                    type: 'success'
+                })
+            }
+            let flashdatadanger = $('.danger-session').data('flashdata');
+            if (flashdatadanger) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error!',
+                    text: flashdatadanger,
+                    type: 'error'
+                })
+            }
+            let table = $('#fund-transfer-table').DataTable({
+                fixedHeader: true,
+                pageLength: 25,
+                processing: true,
+                serverSide: true,
+                responsive: true,
+                ordering: true,
+                dom: '<"d-flex justify-content-between"<<"d-none d-sm-block"B>l>f>rtip',
+                buttons: ["copy", "csv", "excel", "pdf", "print"],
+                language: {
+                    paginate: {
+                        previous: "<",
+                        next: ">"
+                    }
+                },
+                ajax: "{{ route('pengelolaan-kas.fund-transfer.list') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        className: 'align-middle text-center'
+                    },
+                    {
+                        data: 'invoice',
+                        name: 'invoice',
+                        className: 'align-middle text-center'
+                    },
+                    {
+                        data: 'from',
+                        name: 'from',
+                        className: 'align-middle text-center'
+                    },
+                    {
+                        data: 'to',
+                        name: 'to',
+                        className: 'align-middle text-center'
+                    },
+                    {
+                        data: 'total',
+                        name: 'total',
+                        className: 'align-middle text-center'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false,
+                        className: 'align-middle text-center'
+                    },
+
+                ]
+            });
+
+            function reload_table(callback, resetPage = false) {
+                table.ajax.reload(callback, resetPage); //reload datatable ajax 
+            }
+
+            $('#fund-transfer-table').on('click', '.btn-delete', function(e) {
+                let id = $(this).data('id')
+                let invoice = $(this).data('invoice')
+                e.preventDefault()
+                Swal.fire({
+                    title: 'Apakah Yakin?',
+                    text: `Apakah Anda yakin ingin menghapus data transfer dana dengan invoice ${invoice}?`,
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Hapus'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+                        $.ajax({
+                            url: "{{ url('pengelolaan-kas/fund-transfer') }}/" + id,
+                            type: 'POST',
+                            data: {
+                                _token: CSRF_TOKEN,
+                                _method: "delete",
+                            },
+                            dataType: 'JSON',
+                            success: function(response) {
+                                Swal.fire(
+                                    'Deleted!',
+                                    `Data transfer dana dengan invoice ${invoice} berhasil terhapus.`,
+                                    'success'
+                                )
+                                reload_table(null, true)
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                Swal.fire({
+                                    icon: 'error',
+                                    type: 'error',
+                                    title: 'Error saat delete data',
+                                    showConfirmButton: true
+                                })
+                            }
+                        })
+                    }
+                })
+            })
+        })
+    </script>
+@endpush
+
+<x-template-layout>
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4">
+                    @if (session('success'))
+                        <div class="success-session" data-flashdata="{{ session('success') }}"></div>
+                    @elseif(session('danger'))
+                        <div class="danger-session" data-flashdata="{{ session('danger') }}"></div>
+                    @endif
+                    <div class="card-header d-flex justify-content-between pb-0">
+                        <h3>Data Transfer Dana</h3>
+                        <a href="{{ route('pengelolaan-kas.fund-transfer.create') }}" class="btn bg-gradient-primary">
+                            <i class="fas fa-plus-square"></i>
+                        </a>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-3">
+                            <table class="table align-items-center mb-0" id="fund-transfer-table">
+                                <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Nomor</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Invoice</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Dari Bank</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Ke Bank</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Total</th>
+                                        <th class="text-secondary opacity-7"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-template-layout>
