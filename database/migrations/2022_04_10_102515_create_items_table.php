e437dce699d11ddc88e27578d6eb499da2ea5c55
Varez Yeyez <varez.yeyez@gmail.com>
2022-04-16 19:55:27 +0700

    Update Item WIP

---
+++ database/migrations/2022_04_10_102515_create_items_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameItem');
+            $table->string('codeItem')->nullable();
+            $table->string('descriptionItem')->nullable();
+            $table->string('unitItem')->nullable();
+            $table->string('classificationItem')->nullable();
+            $table->string('costItem')->nullable();
+            $table->string('priceItem')->nullable();
+            $table->string('stockItem')->nullable();
+            $table->string('stockMinItem')->nullable();
+            $table->string('pajakPembelianItem')->nullable();
+            $table->string('pajakPenjualanItem')->nullable();
+            $table->string('photoItem')->nullable();
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
