e6ad1518ee7b7528ba55f40055e7e448f08c7949
wisnumutohar18 <91486523+wisnumutohar18@users.noreply.github.com>
2022-05-24 10:06:44 +0700

    p
    pembelian

--- app/Models/ReturPembelian.php
+++ app/Models/ReturPembelian.php
@@ -10,4 +10,19 @@ class ReturPembelian extends Model
     use HasFactory;
     protected $guarded = [];
     protected $table = 'retur_pembelians';
+
+    public function scopeCurrentCompany($query)
+    {
+        return $query->where('company_id', '=', session()->get('company')->id);
+    }
+
+    public function dataContact()
+    {
+        return $this->belongsTo(DataContact::class);
+    }
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class, 'item_pembelian', 'pembelian_id', 'item_id')->withPivot(['id', 'jumlah_barang', 'harga_barang', 'subtotal']);
+    }
 }
