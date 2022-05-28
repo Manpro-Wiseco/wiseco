56b32fb01b9a189147e999df364ac8b0532f95ed
wisnumutohar18 <91486523+wisnumutohar18@users.noreply.github.com>
2022-05-28 09:11:38 +0700

    utang
    utang pembelian table

---
+++ database/migrations/2022_05_28_090620_create_utang_pembelian_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtangPembelianTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utang_pembelian', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->unsignedBigInteger('faktur_pembelian_id');
+            $table->foreign('faktur_pembelian_id')->references('id')->on('faktur_pembelians');
+            $table->integer('jumlah_barang');
+            $table->integer('harga_barang');
+            $table->integer('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utang_pembelian');
+    }
+}
